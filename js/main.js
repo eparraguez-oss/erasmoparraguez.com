4ac7b5d5d2b38fd6b68d25c318fcbb53896780d7
eparraguez-oss <eparraguez@erasmoparraguez.com>
2026-08-03 14:00:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ function initApplication(){
 
     initializeSite();
 
+    // Hora en que se cargó el formulario
+    window.formLoadedTime = Date.now();
     lucide.createIcons();
 
 }
 
 function initializeSite(){
-
     console.info(
         '%cErasmoParraguez.com',
         'color:#0B5FFF;font-weight:700;font-size:14px;'
